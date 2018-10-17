82cd8b80be4b758893b471883089007c8830ace2
cheunghh <254892464@qq.com>
2018-10-17 17:20:45 +0800

    First

---
+++ Web/task4/js/Copy.js
@@ -0,0 +1,88 @@
+function Employee(name, salary) {
+    this.name = name;
+    this.salary = salary;
+    this.show = function(){
+        return "Name: ".concat(this.name, " Salary: ", this.salary);
+    }
+}
+function Manager(name, salary, Inferiors=[]){
+    var employee = new Employee(name, salary);
+    for(var p in employee){
+        this[p] = employee[p];
+    }
+    this.Inferiors = Inferiors;
+    this.getInferiors = function () {
+        var str = this.name + "的下属有：\n";
+        for(var i = 0;i < this.Inferiors.length;i++)
+            str += this.Inferiors[i].show() + "\n";
+        return str;
+    }
+}
+function Secretary(name, salary){
+    var employee = new Employee(name, salary);
+    for(var p in employee){
+        this[p] = employee[p];
+    }
+    this.Superior = null;
+    this.getSuperior = function () {
+        var str = this.name + "的老板是：\n";
+        str += this.Superior.show() + "\n";
+        return str;
+    }
+}
+
+var Se1 = new Secretary("张三", 3000);
+var Se2 = new Secretary("李四", 4000);
+var Se3 = new Secretary("刘五", 5000);
+var Se4 = new Secretary("何六", 6000);
+var Se5 = new Secretary("吴七", 7000);
+var Se6 = new Secretary("黄八", 8000);
+var Ses1 = [Se1, Se2, Se3, Se4];
+var Ses2 = [Se5, Se6];
+var Ma1 = new Manager("路人甲", 11111, Ses1);
+var Ma2 = new Manager("炮灰乙", 22222, Ses2);
+Se1.Superior = Ma1;
+Se2.Superior = Ma1;
+Se3.Superior = Ma1;
+Se4.Superior = Ma1;
+Se5.Superior = Ma2;
+Se6.Superior = Ma2;
+
+function copy_f1(){
+    alert(Se1.show() + "\n" + Se1.getSuperior());
+}
+function copy_f2(){
+    alert(Se2.show() + "\n" + Se2.getSuperior());
+}
+function copy_f3(){
+    alert(Se3.show() + "\n" + Se3.getSuperior());
+}
+function copy_f4(){
+    alert(Se4.show() + "\n" + Se4.getSuperior());
+}
+function copy_f5(){
+    alert(Se5.show() + "\n" + Se5.getSuperior());
+}
+function copy_f6(){
+    alert(Se6.show() + "\n" + Se6.getSuperior());
+}
+function copy_f7(){
+    alert(Ma1.show() + "\n" + Ma1.getInferiors());
+}
+function copy_f8(){
+    alert(Ma2.show() + "\n" + Ma2.getInferiors());
+}
+
+function showPic(){
+    var show = document.getElementById('image');
+    if(show.style.display === "block")
+    {
+        show.style.display = "none";
+        document.getElementById('showPic').innerHTML = "显示关系图";
+    }
+    else
+    {
+        show.style.display = "block";
+        document.getElementById('showPic').innerHTML = "隐藏关系图";
+    }
+}
